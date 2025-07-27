f81710f1d408b1be21fb959b94b9d76837cf9571
rexim <reximkut@gmail.com>
2025-07-27 10:05:03 +0700

    Implement joining

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # Simple lexical analysis for Shell Syntax
 
-Python's shlex.split() but in C
+Python's [shlex](https://docs.python.org/3/library/shlex.html) but in C.
 
 ## Quick Start
+
+### Splitting
 
 ```c
 #include <stdio.h>
@@ -14,9 +16,27 @@ int main(void)
     const char *source = "-C link-args=\"-lm -L.\"";
     Shlex s = {0};
     shlex_init(&s, source, source + strlen(source));
-    while (slex_next(&s)) {
+    while (shlex_next(&s)) {
         printf("%s\n", s.string);
     }
+    shlex_free(&s);
+    return 0;
+}
+```
+
+### Joining
+
+```c
+#include <stdio.h>
+#define SHLEX_IMPLEMENTATION
+#include "shlex.h"
+
+int main(void)
+{
+    Shlex s = {0};
+    shlex_append_quoted(&s, "-C");
+    shlex_append_quoted(&s, "link-args=-lm -L.");
+    printf("%s\n", shlex_join(&s));
     shlex_free(&s);
     return 0;
 }
--- shlex.h
+++ shlex.h
@@ -7,32 +7,84 @@
 #include <string.h>
 #include <ctype.h>
 
+// # The Shlex
+//
+// Both a Lexer and a String Builder which is somewhat POSIX Shell syntax aware.
+//
+// ## Splitting
+//
+// ```c
+// Shlex s = {0};
+// shlex_init(&s, source, source_len);
+// while (shlex_next(&s)) {
+//     printf("%s\n", s.string);
+// }
+// shlex_free(&s);
+// ```
+//
+// ## Joining
+//
+// ```c
+// Shlex s = {0};
+// shlex_append_quoted(&s, "foo");
+// shlex_append_quoted(&s, "bar");
+// shlex_append_quoted(&s, "baz");
+// shlex_append_quoted(&s, "Hello, World");
+// printf("%s\n", shlex_join(&s));
+// shlex_free(&s);
+// ```
 typedef struct {
+    // The source which shlex_next(..) is splitting the tokens from.
     const char *source;
     const char *source_end;
     const char *point;
 
+    // An automatically growing string storage which is used for returning tokens by shlex_next(..)
+    // and collecting joined strings by shlex_append_quoted[_sized](..).
     char *string;
     size_t string_count;
     size_t string_capacity;
 } Shlex;
 
-// Sets the source
+// Sets the source. It also implies shlex_reset(..), so you don't have to call it explicitly.
 void shlex_init(Shlex *s, const char *source, const char *source_end);
 
-// Generally you don't need to free the shlex if you plan to reuse it several times.
-// Just do shlex_init() for each input you need to process and it is going to reuse
-// the memory it allocated every time.
-void shlex_free(Shlex *s);
+// Resets the string storage then chops off a token from the source and appends it to the storage
+// with NULL-terminator so you can access it via s.string as a C string.
+//
+// It also returns s.string as the result on success. Return NULL means ran out of tokens.
+char *shlex_next(Shlex *s);
 
-// Return false means ran out of tokens.
-bool shlex_next(Shlex *s);
+// Resets the state of the shlex removing the source but without deallocating memory of the string
+// storage so it can be reused. You usually wanna use this function before calling
+// shlex_append_quoted[_sized](..) after doing some splitting with shlex_init(..) and shlex_init(..).
+void shlex_reset(Shlex *s);
+
+// Appends a C string to the string storage quoting all the shell unsafe character.
+void shlex_append_quoted(Shlex *s, const char *cstr);
+
+// Same as shlex_append_quoted(..) but for sized strings instead of NULL-terminated ones.
+void shlex_append_quoted_sized(Shlex *s, const char *str, size_t n);
+
+// Appends a NULL-terminator to the string storage, resets the size of the storage and returns the pointer
+// to the C string in the string storage. Usually called after a bunch of calls to
+// shlex_append_quoted[_sized](..) to finalize and acquire the result of appending.
+// Do not keep the result for too long as it will be very quickly invalidated by pretty much any call to shlex_*(..).
+// strdup(..) it to prolong its lifetime.
+char *shlex_join(Shlex *s);
+
+// Deallocates the memory of the string storage and completely zeroes out the shlex.
+// Generally you don't need to free the shlex like that if you plan to reuse it several times.
+// Just do shlex_init(..) or shlex_reset(..) for each input you need to process and it
+// is going to reuse the memory it allocated for the string storage every time.
+void shlex_free(Shlex *s);
 
 #endif // SHLEX_H_
 
 #ifdef SHLEX_IMPLEMENTATION
 
 static void shlex__string_append(Shlex *s, char x);
+static bool shlex__contains_unsafe(const char *str, size_t n);
 
 void shlex_init(Shlex *s, const char *source, const char *source_end)
 {
@@ -47,13 +99,13 @@ void shlex_free(Shlex *s)
     memset(s, 0, sizeof(*s));
 }
 
-bool shlex_next(Shlex *s)
+char *shlex_next(Shlex *s)
 {
     while (s->point < s->source_end && isspace(*s->point)) {
         s->point++;
     }
 
-    if (s->point >= s->source_end) return false;
+    if (s->point >= s->source_end) return NULL;
 
     s->string_count = 0;
     char strlit = 0;
@@ -84,7 +136,7 @@ bool shlex_next(Shlex *s)
                 if (s->point >= s->source_end) {
                     shlex__string_append(s, '\\');
                     shlex__string_append(s, '\0');
-                    return s->string_count > 0;
+                    return s->string;
                 }
 
                 switch (*s->point) {
@@ -127,7 +179,7 @@ bool shlex_next(Shlex *s)
             default:
                 if (isspace(*s->point)) {
                     shlex__string_append(s, '\0');
-                    return true;
+                    return s->string;
                 }
                 shlex__string_append(s, *s->point);
                 s->point++;
@@ -139,7 +191,58 @@ bool shlex_next(Shlex *s)
     }
 
     shlex__string_append(s, '\0');
-    return true;
+    return s->string;
+}
+
+void shlex_reset(Shlex *s)
+{
+    s->source = NULL;
+    s->source_end = NULL;
+    s->point = NULL;
+    s->string_count = 0;
+    // Important! Do not touch s->string_capacity and s->string! They are important for reusage of the allocated memory.
+}
+
+void shlex_append_quoted(Shlex *s, const char *cstr)
+{
+    shlex_append_quoted_sized(s, cstr, strlen(cstr));
+}
+
+void shlex_append_quoted_sized(Shlex *s, const char *str, size_t n)
+{
+    if (s->string_count > 0) shlex__string_append(s, ' ');
+
+    if (n == 0) {
+        shlex__string_append(s, '\'');
+        shlex__string_append(s, '\'');
+        return;
+    }
+
+    if (!shlex__contains_unsafe(str, n)) {
+        for (size_t i = 0; i < n; ++i) {
+            shlex__string_append(s, str[i]);
+        }
+        return;
+    }
+
+    shlex__string_append(s, '\'');
+    for (size_t i = 0; i < n; ++i) {
+        if (str[i] == '\'') {
+            shlex__string_append(s, '\'');
+            shlex__string_append(s, '\'');
+            shlex__string_append(s, '\'');
+        } else {
+            shlex__string_append(s, str[i]);
+        }
+    }
+    shlex__string_append(s, '\'');
+}
+
+char *shlex_join(Shlex *s)
+{
+    shlex__string_append(s, '\0');
+    s->string_count = 0; // Resetting the string storage so it can be reused for more shlex_append_quoted[_sized](..)
+    return s->string;
 }
 
 static void shlex__string_append(Shlex *s, char x)
@@ -155,15 +258,39 @@ static void shlex__string_append(Shlex *s, char x)
     s->string[s->string_count++] = x;
 }
 
+static bool shlex__contains_unsafe(const char *str, size_t n)
+{
+    for (size_t i = 0; i < n; ++i) {
+        char c = str[i];
+        if (!isalnum(c) && strchr("_@%+=:,./-", c) == NULL) {
+            return true;
+        }
+    }
+    return false;
+}
+
 #endif // SHLEX_IMPLEMENTATION
 
 #ifdef SHLEX_SELF_TEST
 
 #include <stdio.h>
 
+void splitting(void);
+void joining(void);
+void splitting_joined(void);
+
 int main(void)
 {
-    const char *sources[] = {
+    splitting();
+    joining();
+    splitting_joined();
+    return 0;
+}
+
+void splitting(void)
+{
+    printf("=== SPLITTING ===\n");
+    static const char *sources[] = {
         "Foo Bar",
         "Foo\\ Bar",
         "Foo\\ \\ Bar",
@@ -176,14 +303,61 @@ int main(void)
     Shlex s = {0};
 
     for (size_t i = 0; i < sources_count; ++i) {
+        if (i > 0) printf("---\n");
         const char *source = sources[i];
         shlex_init(&s, source, source + strlen(source));
         while (shlex_next(&s)) {
-            printf("%s\n", s.string);
+            printf("   %s\n", s.string);
         }
-        printf("------------------------------\n");
     }
-    return 0;
+    printf("\n");
+
+    shlex_free(&s);
+}
+
+void joining(void)
+{
+    printf("=== JOINING ===\n");
+    Shlex s = {0};
+
+    shlex_append_quoted(&s, "foo");
+    shlex_append_quoted(&s, "bar");
+    shlex_append_quoted(&s, "baz");
+    printf("    %s\n", shlex_join(&s));
+
+    shlex_append_quoted(&s, "foo");
+    shlex_append_quoted(&s, "bar baz");
+    printf("    %s\n", shlex_join(&s));
+
+    shlex_append_quoted(&s, "foo");
+    shlex_append_quoted(&s, "bar");
+    shlex_append_quoted(&s, "baz");
+    shlex_append_quoted(&s, "Hello, 'World'");
+    printf("    %s\n", shlex_join(&s));
+
+    printf("\n");
+
+    shlex_free(&s);
+}
+
+void splitting_joined(void)
+{
+
+    printf("=== SPLITTING JOINED ===\n");
+    Shlex s = {0};
+    shlex_append_quoted(&s, "foo");
+    shlex_append_quoted(&s, "bar");
+    shlex_append_quoted(&s, "baz");
+    shlex_append_quoted(&s, "Hello, 'World'");
+    char *source = strdup(shlex_join(&s));
+
+    shlex_init(&s, source, source + strlen(source));
+    while (shlex_next(&s)) {
+        printf("    %s\n", s.string);
+    }
+
+    shlex_free(&s);
+    free(source);
 }
 
 #endif // SHLEX_SELF_TEST
